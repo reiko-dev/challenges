aa28bdf434a7c80b415b607ef2870f72abd14f7e
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-05-13 23:39:51 -0300

    Adds the strokewidth selected by the user

--- fourier_series/lib/pages/complex_dft_user_drawing/user_drawing.dart
+++ fourier_series/lib/pages/complex_dft_user_drawing/user_drawing.dart
@@ -7,15 +7,18 @@ class ComplexDFTUserDrawing extends CustomPainter {
     this.points,
     this.xEpicylePosition,
     this.yEpicylePosition,
+    this.strokeWidth,
   );
 
-  List<List<Offset>> points = [];
+  final List<List<Offset>> points;
   final int xEpicylePosition, yEpicylePosition;
+  final double strokeWidth;
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
       ..color = Colors.white;
 
     points.forEach((points) {
